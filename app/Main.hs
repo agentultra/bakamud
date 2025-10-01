91173657d0c9085c1b7d9db33731f692e569d682
James King <james@agentultra.com>
2025-09-30 20:19:49 -0400

    Modify app/Main.hs
    Fix missing imports, parameters, etc

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,11 @@
 module Main where
 
 import Bakamud.Network.Server
+import Bakamud.Server
 import Bakamud.Server.State
 import qualified Colog.Actions as Log
 
 main :: IO ()
 main = do
-  let initialState = emptyServerState Log.logTextStdout
+  let initialState = emptyServerState Nothing "3000" Log.logTextStdout
   runBakamudServer initialState (runTCPServer Nothing "3000")
