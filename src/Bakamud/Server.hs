1e9ace2f7f0d2addf1975ba9c193b4ec6980dc6e
James King <james@agentultra.com>
2025-10-21 21:13:02 -0400

    Add loginHandler
    Added:
      - loginHandler: handles the login command
      - challenge: always-success auth challenge

--- src/Bakamud/Server.hs
+++ src/Bakamud/Server.hs
@@ -2,6 +2,7 @@
 
 module Bakamud.Server where
 
+import Bakamud.Auth
 import Bakamud.Network.Connection
 import Bakamud.Server.Client
 import Bakamud.Server.Command
@@ -10,6 +11,7 @@ import Bakamud.Server.State
 import Control.Concurrent.STM
 import Control.Monad.IO.Class
 import Control.Monad.Reader
+import qualified Data.Map.Strict as Map
 
 nextConnectionId :: MonadIO m => BakamudServer m ConnectionId
 nextConnectionId = do
@@ -22,5 +24,26 @@ nextConnectionId = do
 dispatchCommand :: MonadIO m => (ConnectionId, Command) -> BakamudServer m ()
 dispatchCommand (connectionId, command) =
   case command of
-    Motd -> put connectionId "THIS IS THE MOTD"
+    Motd -> put connectionId "THIS IS THE MOTD\n"
+    Login user pass -> handleLogin connectionId user pass
     _ -> liftIO $ print (connectionId, command)
+
+handleLogin :: MonadIO m => ConnectionId -> Username -> Password -> BakamudServer m ()
+handleLogin connectionId user pass = do
+  challengeResult <- challenge user pass
+  case challengeResult of
+    ChallengeFail -> put connectionId "Invalid login\n"
+    ChallengeSuccess -> do
+      connectionsTVar <- asks _serverStateConnections
+      liftIO . atomically . (modifyTVar connectionsTVar) $ \connections ->
+        Map.adjust updateAuthSuccess connectionId connections
+      put connectionId "Success!\n"
+  where
+    updateAuthSuccess :: Connection -> Connection
+    updateAuthSuccess connection =
+      connection { _connectionState = Authenticated }
+
+data ChallengeResult = ChallengeSuccess | ChallengeFail deriving (Eq, Show)
+
+challenge :: MonadIO m => Username -> Password -> BakamudServer m ChallengeResult
+challenge _ _ = pure ChallengeSuccess
