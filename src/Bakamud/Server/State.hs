5337da190165237a74b3dca24d5f0387a8ccaef5
James King <james@agentultra.com>
2025-11-11 20:27:01 -0500

    Add time and server time variables

--- src/Bakamud/Server/State.hs
+++ src/Bakamud/Server/State.hs
@@ -5,21 +5,26 @@ import Bakamud.Server.Command
 import Control.Concurrent.STM.TChan
 import Control.Concurrent.STM.TQueue
 import Control.Concurrent.STM.TVar
+import Data.Int
 import Data.Map.Strict (Map)
 import Data.Text (Text)
+import Data.Time.Clock.System
 import Network.Socket
 
 data ServerState m
   = ServerState
-  { _serverStateHostName         :: Maybe HostName
-  , _serverStatePort             :: ServiceName
-  , _serverStateConnectionLimit  :: Word
-  , _serverStateConnectionNextId :: TVar Word
-  , _serverStateConnections      :: TVar (Map ConnectionId Connection)
-  , _serverStateBroadcastChannel :: TChan Text
-  , _serverStateCommandQueue     :: TQueue (ConnectionId, Command)
-  , _serverStateAccounts         :: TVar (Map Username Password)
-  , _serverStateMudMainPath      :: FilePath
+  { _serverStateHostName            :: Maybe HostName
+  , _serverStatePort                :: ServiceName
+  , _serverStateConnectionLimit     :: Word
+  , _serverStateConnectionNextId    :: TVar Word
+  , _serverStateConnections         :: TVar (Map ConnectionId Connection)
+  , _serverStateBroadcastChannel    :: TChan Text
+  , _serverStateCommandQueue        :: TQueue (ConnectionId, Command)
+  , _serverStateAccounts            :: TVar (Map Username Password)
+  , _serverStateMudMainPath         :: FilePath
+  , _serverStateSimStartTime        :: Int64
+  , _serverStateSimTimeRate         :: Int -- ^ How many ms to accumulate before tick, ie: 100ms = 10 ticks/s
+  , _serverStateSimDeltaTimeAccumMs :: Int
   -- ^ Path to main user MUD-code module to run simulation
   }
 
@@ -35,15 +40,19 @@ emptyServerState mHostName serviceName mudMainPath = do
   serverStateAccounts <- newTVarIO $ mempty
   bchan <- newBroadcastTChanIO
   commandQ <- newTQueueIO
+  startTime <- getSystemTime
   pure
     $ ServerState
-    { _serverStateHostName         = mHostName
-    , _serverStatePort             = serviceName
-    , _serverStateConnectionLimit  = 65535
-    , _serverStateConnectionNextId = nextIdTVar
-    , _serverStateConnections      = serverStateTVar
-    , _serverStateBroadcastChannel = bchan
-    , _serverStateCommandQueue     = commandQ
-    , _serverStateAccounts         = serverStateAccounts
-    , _serverStateMudMainPath      = mudMainPath
+    { _serverStateHostName            = mHostName
+    , _serverStatePort                = serviceName
+    , _serverStateConnectionLimit     = 65535
+    , _serverStateConnectionNextId    = nextIdTVar
+    , _serverStateConnections         = serverStateTVar
+    , _serverStateBroadcastChannel    = bchan
+    , _serverStateCommandQueue        = commandQ
+    , _serverStateAccounts            = serverStateAccounts
+    , _serverStateMudMainPath         = mudMainPath
+    , _serverStateSimStartTime        = systemSeconds startTime
+    , _serverStateSimTimeRate         = 100 -- TODO: set this
+    , _serverStateSimDeltaTimeAccumMs = 0
     }
