3ef117060d6de40bab8d7e4ed208fe2fbd1acc27
James King <james@agentultra.com>
2025-12-09 21:14:53 -0500

    Modify src/Bakamud/Server/State.hs
    Added:
      - filepath dependency
      - compileRooms function
    
    Changed:
      - call compileRooms in initServerState

--- src/Bakamud/Server/State.hs
+++ src/Bakamud/Server/State.hs
@@ -1,18 +1,26 @@
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Bakamud.Server.State where
 
 import Bakamud.Network.Connection (ConnectionId, Connection)
 import Bakamud.Server.Command
+import Bakamud.Simulation.Space
 import Control.Concurrent.STM.TChan
 import Control.Concurrent.STM.TQueue
 import Control.Concurrent.STM.TVar
 import Control.Monad
 import Data.Int
+--import Data.List (find)
 import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
 import Data.Text (Text)
+import qualified Data.Text.IO as Text
 import Data.Time.Clock.System
 import Lua (State)
 import qualified Lua.Ersatz.Auxiliary as Lua
 import Network.Socket
+import System.FilePath
 import System.Directory
 
 data ServerState m
@@ -31,6 +39,7 @@ data ServerState m
   , _serverStateSimTimeRate         :: Int64 -- ^ How many ms to accumulate before tick, ie: 100ms = 10 ticks/s
   , _serverStateSimDeltaTimeAccumMs :: TVar Int64
   -- ^ Path to main user MUD-code module to run simulation
+  , _serverStateSimRooms            :: Map Text Room
   }
 
 initServerState
@@ -53,6 +62,7 @@ initServerState mHostName serviceName mudMainPath = do
   luaState <- Lua.hsluaL_newstate
   luaStateTVar <- newTVarIO luaState
   deltaTimeAccumTVar <- newTVarIO 0
+  roomMap <- compileRooms mudMainPath
   pure
     $ ServerState
     { _serverStateHostName            = mHostName
@@ -68,4 +78,19 @@ initServerState mHostName serviceName mudMainPath = do
     , _serverStateSimLastTime         = startTimeTVar
     , _serverStateSimTimeRate         = 1 -- TODO: set this
     , _serverStateSimDeltaTimeAccumMs = deltaTimeAccumTVar
+    , _serverStateSimRooms            = roomMap
     }
+
+-- | Read the mudMainPath directory for Room, Exit definitions and
+-- load them into the state.
+compileRooms :: FilePath -> IO (Map Text Room)
+compileRooms basePath = do
+  roomFiles <- listDirectory $ basePath </> "rooms"
+  Map.fromList <$> traverse loadRoom roomFiles
+  where
+    loadRoom :: FilePath -> IO (Text, Room)
+    loadRoom fname = do
+      source <- Text.readFile $ basePath </> "rooms" </> fname
+      case parseRoomDefinition source of
+        Left err -> error $ "Error loading room definition (" ++ show err ++ "): " ++ fname
+        Right def -> pure def
