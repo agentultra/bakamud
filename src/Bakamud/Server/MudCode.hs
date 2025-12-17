39435237790342e4e5fc03345c864c0f05cd3484
James King <james@agentultra.com>
2025-12-16 21:18:09 -0500

    Modify Bakamud/Server/MudCode.hs
    Add test 'echo' function

--- src/Bakamud/Server/MudCode.hs
+++ src/Bakamud/Server/MudCode.hs
@@ -4,8 +4,21 @@ import Bakamud.Server.Monad
 import Bakamud.Server.State
 import Control.Monad.IO.Class
 import Control.Monad.Reader
+import Foreign.C.String
+import Foreign.C.Types
+import Foreign.Ptr
+import Lua
+import System.FilePath
 
 loadMain :: MonadIO m => BakamudServer m String
 loadMain = do
   mainCodePath <- asks _serverStateMudMainPath
-  liftIO $ readFile mainCodePath
+  liftIO . readFile $ mainCodePath </> "main.lua"
+
+echo :: PreCFunction
+echo state = do
+  s <- lua_tolstring state (nthBottom 1) nullPtr
+  fromLua <- peekCString s
+  withCStringLen ("Hello from Haskell: " ++ fromLua) $ \(cStr, cStrLen) ->
+    lua_pushlstring state cStr (CSize $ fromIntegral cStrLen)
+  return $ NumResults 1
