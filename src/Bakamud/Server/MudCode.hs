0bd04b008bf58271e14373d7309a69ef08f249fd
James King <james@agentultra.com>
2026-02-24 20:24:36 -0500

    Modify Bakamud/Server/MudCode.hs
    Add error return for put_connection

--- src/Bakamud/Server/MudCode.hs
+++ src/Bakamud/Server/MudCode.hs
@@ -11,7 +11,8 @@ import qualified Control.Concurrent.STM.TBQueue as Q
 import Control.Monad.IO.Class
 import Control.Monad.Reader
 import qualified Data.Text.Encoding as Text
-import HsLua.Core
+import HsLua.Core (HaskellFunction, Name)
+import qualified HsLua.Core as Lua
 import qualified StmContainers.Map as SMap
 import System.FilePath
 
@@ -22,19 +23,23 @@ loadMain = do
 
 putConnection :: ServerState -> HaskellFunction e
 putConnection serverState = do
-  mRawConnectionId <- tointeger (nthBottom 1)
-  mMsgBytes <- tostring (nthBottom 2)
+  mRawConnectionId <- Lua.tointeger (Lua.nthBottom 1)
+  mMsgBytes <- Lua.tostring (Lua.nthBottom 2)
   let connections = _serverStateConnections serverState
 
   case (mRawConnectionId, mMsgBytes) of
     (Just rawConnectionId, Just msgBytes) -> do
       let connectionId = ConnectionId $ fromIntegral rawConnectionId
-      liftIO . atomically $ do
+      ok <- liftIO . atomically $ do
         maybeConnection <- SMap.lookup connectionId connections
         case maybeConnection of
-          Nothing -> undefined
-          Just Connection {..} -> Q.writeTBQueue _connectionOutput $ Text.decodeUtf8 msgBytes
-      pure 0
+          Nothing -> pure False
+          Just Connection {..} -> do
+            Q.writeTBQueue _connectionOutput $ Text.decodeUtf8 msgBytes
+            pure True
+      if ok
+        then pure 0
+        else Lua.pushstring "Connection no longer available" *> Lua.error
     _ -> Prelude.error "putConnection: invalid arguments"
 
 exportFunctions :: [(ServerState -> HaskellFunction e, Name)]
