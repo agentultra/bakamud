78b0ca0a372625dc8727af3c1a953fd563839f4d
James King <james@agentultra.com>
2026-03-03 21:35:30 -0500

    Modify Bakamud/User.hs
    Add userName field

--- src/Bakamud/User.hs
+++ src/Bakamud/User.hs
@@ -1,12 +1,18 @@
 module Bakamud.User where
 
 import Data.Int
+import Data.Text (Text)
 import qualified Database.SQLite.Simple as DB
 
-newtype User = User { _userId :: Int64 }
+data User
+  = User
+  { _userId   :: Int64
+  , _userName :: Text
+  }
+  deriving (Eq, Show)
 
 instance DB.ToRow User where
-  toRow (User userId) = DB.toRow (DB.Only userId)
+  toRow (User userId userName) = DB.toRow (userId, userName)
 
 instance DB.FromRow User where
-  fromRow = User <$> DB.field
+  fromRow = User <$> DB.field <*> DB.field
